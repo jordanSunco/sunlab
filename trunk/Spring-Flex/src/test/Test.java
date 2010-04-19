3b1bdf4ff6f90de5af1c24e6a284be4ac0cd103e
klsforever@gmail.com <klsforever@gmail.com@42ffc310-564a-11de-8dc1-e798e446b60a>
2010-04-19 08:51:14 +0000

    1. 直接使用BlazeDS的后端, Flex 可以通过修改编译参数-services "D:\workspace\Spring-Flex\WebContent\WEB-INF\flex\services-config.xml"而不需要在代码中指定通信渠道 2. 使用了Spring-Flex(Spring BlazeDS Integration)后, 不再有用, 必须在代码中指定ChannelSet

---
+++ trunk/Spring-Flex/src/test/Test.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright
+ */
+
+package test;
+
+/**
+ * 
+ * @author Sun
+ * @version Test.java 2010-4-19 下午03:50:54
+ */
+public class Test {
+    private String s;
+
+    /**
+     * @return the s
+     */
+    public String getS() {
+        return this.s;
+    }
+
+    /**
+     * @param s the s to set
+     */
+    public void setS(String s) {
+        this.s = s;
+    }
+}
