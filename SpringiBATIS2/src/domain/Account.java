8fe1d90722634f71c829ea43e401040fcc47614c
klsforever@gmail.com <klsforever@gmail.com@42ffc310-564a-11de-8dc1-e798e446b60a>
2010-05-25 05:39:25 +0000

    Spring 与 iBATIS 2 集成(ORM)访问H2数据库

---
+++ SpringiBATIS2/src/domain/Account.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright
+ */
+
+package domain;
+
+/**
+ * Domain
+ * 
+ * @author Sun
+ * @version Account.java 2010-5-24 下午05:12:26
+ */
+public class Account {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the emailAddress
+     */
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+    /**
+     * @param emailAddress the emailAddress to set
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
