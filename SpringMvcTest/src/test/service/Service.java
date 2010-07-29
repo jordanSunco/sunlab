04797cc17d3dfabc63d6d9cac33ecb25e3b4db59
klsforever@gmail.com <klsforever@gmail.com@42ffc310-564a-11de-8dc1-e798e446b60a>
2010-07-29 04:02:25 +0000

    1. 使用Spring MVC 2.5.6 做的简单例子, 快速熟悉Spring MVC的开发模式 2. 包含使用Spring-Test来完成Spring的单元测试, 注意使用mock类来模拟环境, 从而摆脱对环境的依赖, 如Servlet Container

---
+++ SpringMvcTest/src/test/service/Service.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright
+ */
+
+package test.service;
+
+/**
+ * 服务测试类
+ * 
+ * @author Sun
+ * @version Service.java 2010-7-26 下午10:51:14
+ */
+public class Service {
+    private String hello;
+
+    /**
+     * @return the hello
+     */
+    public String getHello() {
+        return this.hello;
+    }
+
+    /**
+     * @param hello the hello to set
+     */
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
