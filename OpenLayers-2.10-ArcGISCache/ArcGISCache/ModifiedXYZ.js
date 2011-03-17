cdb5cdf7322f15392d3e7ca216fc46c3a2d8ab98
klsforever@gmail.com <klsforever@gmail.com@42ffc310-564a-11de-8dc1-e798e446b60a>
2011-03-17 07:32:04 +0000

    根据trunk/openlayers中最新的代码修改获取基于tile origin来获取瓦片XYZ的逻辑

--- OpenLayers-2.10-ArcGISCache/ArcGISCache/ModifiedXYZ.js
+++ OpenLayers-2.10-ArcGISCache/ArcGISCache/ModifiedXYZ.js
@@ -1,40 +1,66 @@
 OpenLayers.Layer.XYZ.Modified = OpenLayers.Class(OpenLayers.Layer.XYZ, {
-    getURL: function (bounds) {
+    /**
+     * 由于一些切片是根据tile origin而非max extent来计算的.
+     * 因此只需用tileOrigin替换maxExtent即可.
+     * With gridded tile systems, sometimes the tile origin is far from the actual maxExtent of the data.
+     * For example, when a "google-like" tiling scheme is used with local data.
+     * This type of setup is also common with ESRI tile caches.
+     */
+    getXYZ: function(bounds) {
         var res = this.map.getResolution();
-        var x = Math.round((bounds.left - this.tileOrigin.lon) 
+        var x = Math.round((bounds.left - this.tileOrigin.lon)
             / (res * this.tileSize.w));
-        var y = Math.round((this.tileOrigin.lat - bounds.top) 
+        var y = Math.round((this.tileOrigin.lat - bounds.top)
             / (res * this.tileSize.h));
-        var z = this.map.getZoom();
+        var z = this.serverResolutions != null ?
+            OpenLayers.Util.indexOf(this.serverResolutions, res) :
+            this.map.getZoom() + this.zoomOffset;
 
+        return {'x': x, 'y': y, 'z': z};
+    },
+
+    /**
+     * 在最新版本(trunk/openlayers)中, 已经有人提取了getURL如何获取x, y, z的逻辑,
+     * 子类只需重写getXYZ即可, 这里只是将最新版的getURL照搬过来, 没有进行扩展.
+     * 
+     * TODO 因此当新版本中采用getXYZ后, 不需要重写此方法, 只需要重写getXYZ
+     */
+    getURL: function (bounds) {
+        var xyz = this.getXYZ(bounds);
         var url = this.url;
-        var s = '' + x + y + z;
-        if (url instanceof Array)
-        {
+        if (url instanceof Array) {
+            var s = '' + xyz.x + xyz.y + xyz.z;
             url = this.selectUrl(s, url);
         }
-        
-        var path = OpenLayers.String.format(url, {'x': x, 'y': y, 'z': z});
 
-        return path;
+        return OpenLayers.String.format(url, xyz);
     },
+
+    /**
+     * 在最新版本(trunk/openlayers)中, 已经有人修改了initGriddedTiles如何获取origin的逻辑.
+     * 以前(2.10)是直接获得maxExtent, 再计算calculateGridLayout(bounds, extent, resolution),
+     * 如今origin通过一个getTileOrigin来获取, 判断有tileOrigin则返回tileOrigin,
+     * 无则根据maxExtent来计算出origin, 再计算calculateGridLayout(bounds, origin, resolution).
+     * 
+     * TODO 因此当新版本中采用getTileOrigin后, 不需要重写此方法
+     */
     calculateGridLayout: function(bounds, extent, resolution) {
         var tilelon = resolution * this.tileSize.w;
         var tilelat = resolution * this.tileSize.h;
-        
+
         var offsetlon = bounds.left - this.tileOrigin.lon;
         var tilecol = Math.floor(offsetlon/tilelon) - this.buffer;
         var tilecolremain = offsetlon/tilelon - tilecol;
         var tileoffsetx = -tilecolremain * this.tileSize.w;
         var tileoffsetlon = this.tileOrigin.lon + tilecol * tilelon;
-        
-        var offsetlat = this.tileOrigin.lat - bounds.top + tilelat; 
-        var tilerow = Math.floor(offsetlat/tilelat) - this.buffer;
+
+        var offsetlat = bounds.top - (this.tileOrigin.lat + tilelat);
+        var tilerow = Math.ceil(offsetlat/tilelat) + this.buffer;
         var tilerowremain = tilerow - offsetlat/tilelat;
-        var tileoffsety = tilerowremain * this.tileSize.h;
-        var tileoffsetlat = this.tileOrigin.lat - tilelat*tilerow;
-        
-        return { 
+        var tileoffsety = -tilerowremain * this.tileSize.h;
+        var tileoffsetlat = this.tileOrigin.lat + tilerow * tilelat;
+
+        return {
           tilelon: tilelon, tilelat: tilelat,
           tileoffsetlon: tileoffsetlon, tileoffsetlat: tileoffsetlat,
           tileoffsetx: tileoffsetx, tileoffsety: tileoffsety
