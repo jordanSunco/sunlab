af3ddcee344593cd0f7230910e434effac081463
klsforever@gmail.com <klsforever@gmail.com@42ffc310-564a-11de-8dc1-e798e446b60a>
2011-06-01 15:19:08 +0000

    测试通过Worker实现JavaScript多线程, 将一些复杂运算直接放在客户端后台计算, 不需要通过XHR借助后端服务的异步机制.

---
+++ WebWorkersDemo/sum.js
@@ -0,0 +1,10 @@
+﻿/**
+ * 计算从1 ~ max的累加总数, 例如 1 + 2 + 3 + ... + 100 = 5050
+ */
+function sum(max) {
+    var _sum = 0;
+    for (var i = 1; i <= max; i++) {
+        _sum += i;
+    }
+    return _sum;
+}
---
+++ WebWorkersDemo/worker.js
@@ -0,0 +1,15 @@
+﻿importScripts("sum.js");
+
+/**
+ * 得到主线程传过的消息, 启动线程进行大数据计算.
+ * mainThread.postMessage -> workerThread.onmessage
+ */
+onmessage = function (event) {
+    var a = this;
+    // 从event.data中取得主线程传来的数据
+    var max = event.data;
+    // 将结果传递给主线程
+    // 不可进行访问/操作主线程界面(DOM)数据, 即使通过postMessage传递过来
+    // workerThread.postMessage -> mainThread.onmessage
+    postMessage(sum(max));
+};
